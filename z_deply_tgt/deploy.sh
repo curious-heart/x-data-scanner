291b07ae47db808e47bf1aece43297a40af28980
alientek <example@email.com>
2025-07-19 21:25:58 +0800

    update deploy related files.

--- z_deply_tgt/deploy.sh
+++ z_deply_tgt/deploy.sh
@@ -6,3 +6,17 @@ export PATH=/media/kylin/49487059-c39d-4e34-b26a-a2bd363fee4b/16.back-scattering
 export LD_LIBRARY_PATH=/media/kylin/49487059-c39d-4e34-b26a-a2bd363fee4b/16.back-scattering/00.env/qt5.15.2-src/qt-everywhere-src-5.15.2/qtbase/lib:$LD_LIBRARY_PATH
 
 $DEPLOY_APP $1 -appimage -no-copy-copyright-files
+
+#now create the run.sh
+pth=$(dirname "$1")
+app_name=$(basename "$1")
+run_sh_fpn="$pth/run.sh"
+
+cat > $run_sh_fpn << EOF
+#!/bin/sh
+export QT_XCB_GL_INTEGRATION=none
+EOF
+
+echo "./$app_name" >> $run_sh_fpn
+
+chmod +x $run_sh_fpn
