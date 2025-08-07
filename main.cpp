9d558726f5ca0221dc9f3a33f665ae6d3c33fe3a
curious-heart <onground@163.com>
2025-08-07 19:57:09 +0800

    Adjust app font size automatically.

--- main.cpp
+++ main.cpp
@@ -5,10 +5,44 @@
 #include <QThread>
 #include <QString>
 #include <QMessageBox>
+#include <QScreen>
 
 #include "common_tools/common_tool_func.h"
 #include "logger/logger.h"
 #include "sysconfigs/sysconfigs.h"
+
+void set_app_font_size()
+{
+    static qreal ls_base_scale_factor = 2;
+
+    QScreen *screen = QGuiApplication::primaryScreen();
+    QSize scrn_size = screen->size();
+    int scrn_width_px = scrn_size.width();
+    int scrn_height_px = scrn_size.height();
+    qreal dpiX = screen->logicalDotsPerInchX();
+    qreal dpiY = screen->logicalDotsPerInchY();
+
+    qreal phy_dpiX = screen->logicalDotsPerInchX();
+    qreal phy_dpiY = screen->logicalDotsPerInchY();
+
+    qreal scale_factor = screen->devicePixelRatio();
+
+    QString scrn_info_str;
+    scrn_info_str += QString("scrn width in px: %1, heigh in px: %2\n").arg(scrn_width_px).arg(scrn_height_px);
+    scrn_info_str += QString("scrn logical dpi X:%1, Y: %2\n").arg(dpiX).arg(dpiY);
+    scrn_info_str += QString("scrn physical dpi X:%1, Y: %2\n").arg(phy_dpiX).arg(phy_dpiY);
+    scrn_info_str += QString("scale factor: %1\n").arg(scale_factor);
+
+    DIY_LOG(LOG_INFO, scrn_info_str);
+
+    QFont app_font = QApplication::font();
+    qreal ori_app_font_pt_size = app_font.pointSizeF();
+    qreal new_app_font_pt_size = ori_app_font_pt_size * ls_base_scale_factor / scale_factor;
+    app_font.setPointSizeF(new_app_font_pt_size);
+    QApplication::setFont(app_font);
+    DIY_LOG(LOG_INFO, QString("ori app font point size is %1, new size is %2\n")
+                        .arg(ori_app_font_pt_size).arg(new_app_font_pt_size));
+}
 
 int main(int argc, char *argv[])
 {
@@ -35,6 +69,8 @@ int main(int argc, char *argv[])
     }
     //use the log level defined in config file.
     update_log_level((LOG_LEVEL)(g_sys_configs_block.log_level));
+
+    set_app_font_size();
 
     /*create main window.*/
     QString title_str = QString("%1 %2").arg(a.applicationName(), APP_VER_STR);
